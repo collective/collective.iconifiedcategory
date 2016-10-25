a83319e12b827d43cc1b522be530e089f9d412f7
Gauthier Bastien <g.bastien@imio.be>
2016-10-25 11:01:47 +0200

    Added test for warn filesize displayed in the childs view

--- src/collective/iconifiedcategory/tests/test_views.py
+++ src/collective/iconifiedcategory/tests/test_views.py
@@ -17,6 +17,13 @@ class TestCategorizedChildView(BaseTestCase):
         self.assertTrue('<a href="http://nohost/plone/file/@@download/file/file.txt">' in result)
         self.assertTrue('<a href="http://nohost/plone/image/@@download/file/icon1.png">' in result)
 
+        # in case a file is too large, a warning is displayed
+        # manipulate stored categorized_elements
+        self.portal.categorized_elements[self.portal['file'].UID()]['warn_filesize'] = True
+        self.portal.categorized_elements[self.portal['file'].UID()]['filesize'] = 7000000
+        self.assertTrue("(<span class=\'warn_filesize\' title=\'Annex size is huge, "
+                        "it could be difficult to be downloaded!\'>6.7 MB</span>)" in view())
+
         # remove the categorized elements
         api.content.delete(self.portal['file'])
         api.content.delete(self.portal['image'])
